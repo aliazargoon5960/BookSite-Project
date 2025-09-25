0e154d956ece67491a097fb56ebcea9b82be222b
Ali Asghar Azargoon <aliasgharazargoon5960@gmail.com>
2025-09-25 19:17:57 +0330

    change views.py

--- books/views.py
+++ books/views.py
@@ -53,6 +53,3 @@ class AuthorDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['books'] = Book.objects.filter(author=self.object)
         return context
-
-def Hello(request):
-    return redirect('books:home')
